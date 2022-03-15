5a670f28db084746fd9afbcc4f0a1849edf6d290
marah-jaradat <marahjaradat97@gmail.com>
2022-03-15 22:11:36 +0300

    add6

--- javascript/linked-list/ll.js
+++ javascript/linked-list/ll.js
@@ -49,5 +49,33 @@ class LinkedList {
       console.error("include is invalid");
     }
   }
+
+  append(value) {
+    const anotherNode = new Node(value);
+    if (!this.head) {
+      this.head = anotherNode;
+    }
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    current.next = anotherNode;
+  }
+  insertBefore(preValue, newValue) {
+    let newnode = new Node(newValue);
+    let current = this.head;
+    if (preValue === this.head.value) {
+      this.insert(newValue);
+      return this.head;
+    }
+    while (current.next !== null) {
+      if (current.next.value === preValue) {
+        newnode = current.next;
+        current.next = newnode;
+        return this.head;
+      }
+      current = current.next;
+    }
+  }
 }
 module.exports = LinkedList;
