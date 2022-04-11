3fe8cb31438981a03839c25d1919647377267a47
marah-jaradat <marahjaradat97@gmail.com>
2022-04-11 22:34:44 +0300

    challenge 17 solved

--- javascript/tree/js/breadth.js
+++ javascript/tree/js/breadth.js
@@ -1,35 +1,57 @@
 "use srict";
 
-// const node = require("./node");
+// const Node = require("./node");
+const BinaryTree = require("./binarytree");
 
-class BreadthTree {
-  constructor(root = null) {
-    this.root = root;
-  }
-  breadth() {
+class Breadth extends BinaryTree {
+  BreadthiFrst() {
     let arr = [];
-    let queue = [];
-    queue.pushItem(this.root);
-
-    while (queue.length > 0) {
-      let item = queue.shift();
-
-      arr.pushItem(item.value);
-
-      if (item.left === null && item.right === null) {
-        continue;
+    let qArr = [];
+    qArr.pushItem(this.root);
+    while (qArr.length > 0) {
+      let current = qArr.shift();
+      arr.pushItem(current.value);
+      if (current.left !== null) {
+        qArr.pushItem(current.left);
       }
-      if (item.left !== null) {
-        queue.pushItem(item.left);
-      }
-      if (item.right !== null) {
-        queue.pushItem(item.right);
+      if (current.right !== null) {
+        qArr.pushItem(current.right);
       }
     }
-    if (queue === null) {
-      return;
-    }
+    return arr;
   }
 }
 
-module.exports = BreadthTree;
+module.exports = Breadth;
+
+// class BreadthTree {
+//   constructor(root = null) {
+//     this.root = root;
+//   }
+//   breadth() {
+//     let arr = [];
+//     let queue = [];
+//     queue.enqueue(this.root);
+
+//     while (queue.length > 0) {
+//       let item = queue.shift();
+
+//       arr.enqueue(item.value);
+
+//       if (item.left === null && item.right === null) {
+//         continue;
+//       }
+//       if (item.left !== null) {
+//         queue.enqueue(item.left);
+//       }
+//       if (item.right !== null) {
+//         queue.enqueue(item.right);
+//       }
+//     }
+//     if (queue === null) {
+//       return;
+//     }
+//   }
+// }
+
+// module.exports = BreadthTree;
