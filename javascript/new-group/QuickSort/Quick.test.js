0b999fbc63d35f9725599b8416bf3409d99536b1
marah-jaradat <marahjaradat97@gmail.com>
2022-05-16 17:47:19 +0300

    adding ch28

--- javascript/new-group/QuickSort/Quick.test.js
+++ javascript/new-group/QuickSort/Quick.test.js
@@ -10,18 +10,18 @@ describe("quick Sort check", () => {
   });
 
   it("test nearly sorted", () => {
-    const arr = [4, 8, 15, 16, 42, 23];
+    const arr = [2, 3, 5, 7, 13, 11];
     quickSort(arr, 0, arr.length - 1);
-    expect(arr).toEqual([4, 8, 15, 16, 23, 42]);
+    expect(arr).toEqual([2, 3, 5, 7, 11, 13]);
   });
   it("test reversed", () => {
-    const arr = [42, 23, 16, 15, 8, 4];
+    const arr = [20, 18, 12, 8, 5, -2];
     quickSort(arr, 0, arr.length - 1);
-    expect(arr).toEqual([4, 8, 15, 16, 23, 42]);
+    expect(arr).toEqual([-2, 5, 8, 12, 18, 20]);
   });
   it("test few uniques", () => {
-    const arr = [8, 4, 4, 23, 23, 42, 16, 16, 15];
+    const arr = [5, 12, 7, 5, 5, 7];
     quickSort(arr, 0, arr.length - 1);
-    expect(arr).toEqual([4, 4, 8, 15, 16, 16, 23, 23, 42]);
+    expect(arr).toEqual([5, 5, 5, 7, 7, 12]);
   });
 });
