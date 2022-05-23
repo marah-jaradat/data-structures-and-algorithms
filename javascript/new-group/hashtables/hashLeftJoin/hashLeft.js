e32d93b8f6443493183c3800a3bcccafcb12539a
marah-jaradat <marahjaradat97@gmail.com>
2022-05-23 17:27:45 +0300

    first add to ch 33D

---
+++ javascript/new-group/hashtables/hashLeftJoin/hashLeft.js
@@ -0,0 +1,14 @@
+function hashMapLeftJoin(map1, map2) {
+  let result = [];
+
+  map1.forEach((value, key) => {
+    let entry = [key, value];
+    map2.get(key) ? entry.push(map2.get(key)) : entry.push(null);
+
+    result.push(entry);
+  });
+
+  return result;
+}
+
+module.exports = hashMapLeftJoin;
