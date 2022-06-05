63fc720f2499fa23dd5d8cc08d87dd21bef03054
marah-jaradat <marahjaradat97@gmail.com>
2022-06-05 23:19:14 +0300

    adding 38

--- javascript/graphs/graph.js
+++ javascript/graphs/graph.js
@@ -9,6 +9,7 @@ class Graph {
   addNode(node) {
     return this.adjList.set(node, []);
   }
+
   addEdge(node1, node2, weight) {
     node1 || node2
       ? this.adjList.get(node1).push(new Edge(node2, weight))
@@ -48,6 +49,36 @@ class Graph {
       });
     }
     return visited;
+  }
+
+  DepthFirst(start) {
+    if (this.adjList.size === 0) {
+      return "empty";
+    }
+    if (!this.adjList.has(start)) {
+      return null;
+    }
+    let visitedNodes = [];
+    let stack = [];
+    const traverse = (node, visitedNodes) => {
+      if (!node) return;
+      visitedNodes.push(node);
+      if (!stack.includes(node)) {
+        stack.push(node);
+      }
+      const neighbors = this.getNeioughbors(node);
+      console.log("neighbors", neighbors);
+      for (let neighbor of neighbors) {
+        // console.log("neighbor", neighbor);
+        if (!visitedNodes.includes(neighbor.vertex)) {
+          traverse(neighbor.vertex, visitedNodes);
+        }
+      }
+    };
+
+    traverse(start, visitedNodes);
+
+    return stack;
   }
 }
 module.exports = Graph;
---
+++ javascript/graphs/readme/depthFirst.md
@@ -0,0 +1,20 @@
+# graph-depth-first
+
+Conduct a depth first preorder traversal on a graph
+
+## Features
+
+- Write the following method for the Graph class:
+
+    Name: Depth first
+    Arguments: Node (Starting point of search)
+    Return: A collection of nodes in their pre-order depth-first traversal order
+    Program output: Display the collection
+  
+- write tests for every possible senario
+
+### Efficiency
+
+| method|Time complexity |Space complexity | 
+| :---: | :---: | :---: |
+|graph-depth-first|O(n)| O(n)|
